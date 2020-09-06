22f78dd0f1a435bb85c865134a885d4acc4dc859
Jason Law <jasonlaw.dev@gmail.com>
2020-09-06 11:12:50 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-plusplus': 0,
+    'no-await-in-loop': 0,
+    'func-names': 0,
+    'no-console': 0,
+  },
+};
